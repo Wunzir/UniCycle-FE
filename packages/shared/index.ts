ca035d0e246b2f058e1b946e3a94ca9fe4ee5575
Omer Gafar <gafaro@rpi.edu>
2026-04-28 19:29:32 -0400

    add shared listing types package and file restrcuture for listing.ts

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -1,8 +1,1 @@
-export const SHARED_TEXT = "Hello from the Shared Library (TypeScript)!";
-
-export interface User {
-    id: string;
-    username: string;
-    email: string;
-    role: 'student' | 'admin';
-}
+export * from "./listings";
